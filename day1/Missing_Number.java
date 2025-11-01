a350ddbe44049cc263bc7288d8508ac5ebfc5870
Sakshi Singh <sakshi.singh171101@gmail.com>
2025-11-01 18:26:43 +0000

    Auto upload day2 – 2025-11-01 18:26

---
+++ day1/Missing_Number.java
@@ -0,0 +1,8 @@
+// Problem: Missing Number
+// Link: https://leetcode.com/problems/missing-number/
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+    }
+}
---
+++ day2/Valid_Anagram.java
@@ -0,0 +1,8 @@
+// Problem: Valid Anagram
+// Link: https://leetcode.com/problems/valid-anagram/
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+    }
+}
