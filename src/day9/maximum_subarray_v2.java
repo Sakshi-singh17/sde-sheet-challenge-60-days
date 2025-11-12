9f5871a38daa90de3076a9cb238f98c11232b611
Sakshi Singh <sakshisingh@example.com>
2025-11-12 18:14:10 +0000

    Auto upload LeetCode solutions – Wed Nov 12 18:14:10 UTC 2025

---
+++ src/day9/maximum_subarray_v2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums.length==0){
+            return -1;
+        }
+        if(nums.length==1){
+            return nums[0];
+        }
+        int sum=0;
+        int maxS=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length; i++){
+            sum+=nums[i];
+            if(sum>maxS){
+                maxS=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxS;
+    }
+}
